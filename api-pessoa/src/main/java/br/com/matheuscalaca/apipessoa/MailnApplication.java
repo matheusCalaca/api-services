96460d32fff4a78863ad78473686693b5db6520f
matheus Calaça <matheusfcalaca@gmail.com>
2020-05-15 17:58:05 -0300

    criado pessoa api

---
+++ api-pessoa/src/main/java/br/com/matheuscalaca/apipessoa/MailnApplication.java
@@ -0,0 +1,13 @@
+package br.com.matheuscalaca.apipessoa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MailnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MailnApplication.class, args);
+	}
+
+}
